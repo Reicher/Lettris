50bded5d4ca29dd337bf517dc98f1386fb80ecac
Reicher <robin.o.reicher@gmail.com>
2017-10-25 21:14:19 +0200

    Whole alphabet + cleanup
    Signed-off-by: Reicher <robin.o.reicher@gmail.com>

--- src/box.js
+++ src/box.js
@@ -1,21 +1,17 @@
-Box = function (game, letter, x = 0, y = 0) {
+Box = function (game, x = 0, y = 0) {
     Phaser.Sprite.call(this, game, x, y, 'box')
 
+    // Physics (Needs a lot of tinkering)
     game.physics.p2.enable(this);
     this.body.collideWorldBounds = true
     this.body.fixedRotation = true;
 
-    // Letter
+    // Letter (should probably not be completely random)
+    var letter = game.rnd.pick("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
     var style = { font: "30px Arial", fill: "#000000" }
-
-    var text = this.game.add.text(0,
-				  0,
-				  letter,
-				  style)
+    var text = this.game.add.text(0, 0, letter, style)
     text.anchor.setTo(0.5)
     this.addChild(text)
-
-    game.add.existing(this)
 }
 
 Box.prototype = Object.create(Phaser.Sprite.prototype);
--- src/game.js
+++ src/game.js
@@ -3,9 +3,7 @@ Lettris.Game = function (game) {
 
 Lettris.Game.prototype = {
     create: function (game) {
-	console.log("GAME ON!")
-
-	//  Enable p2 physics
+	// Enable p2 physics (Needs tinkering around)
 	game.physics.startSystem(Phaser.Physics.P2JS);
 	game.physics.p2.gravity.y = 300;
 	game.physics.p2.restitution = 0.1;
@@ -18,22 +16,13 @@ Lettris.Game.prototype = {
     },
 
     spawn_random_box: function () {
-	var letter = this.game.rnd.pick(['A',
-					 'B',
-					 'C',
-					 'D',
-					 'E',
-					 'F'])
-
 	var pos = this.game.rnd.integer()%this.game.width
 
 	this.boxes.add(new Box(this.game,
-			       letter,
 			       pos))
     },
 
     update: function () {
-	//this.game.physics.arcade.collide(this.boxes);
     },
 
     render: function() {
