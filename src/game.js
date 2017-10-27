c95b2969cc327b90a487b20cafd98f8a8fb18ad2
Reicher <robin.o.reicher@gmail.com>
2017-10-28 01:53:42 +0200

    Revert "More work on the grid, getting there"
    This reverts commit d511f52aec371184f5eff02c5881b443a56abe4c.

--- src/game.js
+++ src/game.js
@@ -4,18 +4,33 @@ Lettris.Game = function (game) {
 Lettris.Game.prototype = {
     create: function (game) {
 	this.boxes = game.add.group();
-
-	this.grid = new Grid(game, 6)
+	this.create_grid(6);
 
 	game.time.events.loop(Phaser.Timer.SECOND * 2,
 			      this.spawn_random_box,
 			      this)
     },
 
+    create_grid: function(width) {
+	var size = this.game.width / width
+	var height = Math.floor(this.game.height / size)
+	this.grid = []
+	for(var i=0; i<width; i++) {
+	    this.grid[i] = new Array(height);
+	    for(var j=0; j<height; j++) {
+		var box = new Box(this.game, 20 + i * 40, 20 + j * 40);
+		this.grid[i][j] = box
+		this.boxes.add(box)
+	    }
+	}
+	return this.grid
+    },
+
     spawn_random_box: function () {
-	var column = this.game.rnd.integer() % this.grid.columns
-	var box = this.grid.addBox(column)
-	this.boxes.add(box)
+	var pos = this.game.rnd.integer()%this.game.width
+
+	this.boxes.add(new Box(this.game,
+			       pos))
     },
 
     update: function () {
