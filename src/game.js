ecf3d27d5ecb697abfe1f0c9523b3650dccad5f1
Reicher <robin.o.reicher@gmail.com>
2017-10-28 01:53:44 +0200

    Revert "Remove all physics, started with a grid system"
    This reverts commit 187ec902bcdf1f9a87155c604c3e6d6dbd0073a7.

--- src/game.js
+++ src/game.js
@@ -3,27 +3,19 @@ Lettris.Game = function (game) {
 
 Lettris.Game.prototype = {
     create: function (game) {
+
+	game.world.setBounds(0, -40, game.width, game.height);
+
+	// Enable p2 physics (Needs tinkering around)
+	game.physics.startSystem(Phaser.Physics.P2JS);
+	game.physics.p2.gravity.y = 300;
+	game.physics.p2.restitution = 0.05
+
 	this.boxes = game.add.group();
-	this.create_grid(6);
 
 	game.time.events.loop(Phaser.Timer.SECOND * 2,
 			      this.spawn_random_box,
 			      this)
-    },
-
-    create_grid: function(width) {
-	var size = this.game.width / width
-	var height = Math.floor(this.game.height / size)
-	this.grid = []
-	for(var i=0; i<width; i++) {
-	    this.grid[i] = new Array(height);
-	    for(var j=0; j<height; j++) {
-		var box = new Box(this.game, 20 + i * 40, 20 + j * 40);
-		this.grid[i][j] = box
-		this.boxes.add(box)
-	    }
-	}
-	return this.grid
     },
 
     spawn_random_box: function () {
