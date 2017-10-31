2b80452f16b412cc1fa806ea6ef71d5f9ec9218f
Reicher <robin.o.reicher@gmail.com>
2017-10-31 17:01:50 +0100

    Add points score for each letter. Helps the player orientate the letters!
    Signed-off-by: Reicher <robin.o.reicher@gmail.com>

--- src/box.js
+++ src/box.js
@@ -10,12 +10,22 @@ Box = function (game, x, boxClicked) {
     var letterKey = game.rnd.pick("abcdefghijklmnopqrstuvwxyz")
     var letter = game.cache.getJSON('let-eng-std').letters[letterKey]
 
+    this.points = letter.points;
+
+    // letter text
     var style = { font: "30px Arial", fill: "#000000" }
     this.text = this.game.add.text(0, 0, letterKey, style)
     this.text.anchor.setTo(0.5)
     this.addChild(this.text)
 
-    this.points = letter.points;
+    // points text
+    var style = { font: "10px Arial", fill: "#000000" }
+    var point_text = this.game.add.text(this.width/2  - 3,
+					this.height/2 + 4,
+					this.points,
+					style)
+    point_text.anchor.setTo(1)
+    this.addChild(point_text)
 
     // Interaction
     this.inputEnabled = true;
