cfd92c7e650f391fd7cc38ef5422e280bf6ca2fb
Reicher <robin.o.reicher@gmail.com>
2017-11-05 02:25:02 +0100

    fullscreen mode
    Signed-off-by: Reicher <robin.o.reicher@gmail.com>

--- src/boot.js
+++ src/boot.js
@@ -8,10 +8,9 @@ Lettris.Boot.prototype = {
 	create: function () {
 		// set scale options
 		this.input.maxPointers = 1;
-		// this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-		// this.scale.pageAlignHorizontally = true;
-		// this.scale.pageAlignVertically = true;
-		// this.stage.smoothed = false;
+		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		this.scale.pageAlignHorizontally = true;
+		this.scale.pageAlignVertically = true;
 
 		// start the Preloader state
 		this.state.start('Preloader');
