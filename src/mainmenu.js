d5fb2c9a98c810684ea7c2bcfea43fa629e1e64a
Reicher <robin.o.reicher@gmail.com>
2017-11-17 00:27:12 +0100

    o.7 Beta!
    Signed-off-by: Reicher <robin.o.reicher@gmail.com>

--- src/mainmenu.js
+++ src/mainmenu.js
@@ -16,7 +16,7 @@ Lettris.MainMenu.prototype = {
 	var style = { font: "10px Arial", fill: "#FF00F0"}
 	this.version = this.game.add.text(this.game.width-5,
 					  this.game.height-5,
-					  "0.6 Beta",
+					  "0.7 Beta",
 					  style)
 	this.version.anchor.setTo(1, 1)
     },
