8693463e146310c7eb7856f2083367a9ad8a8276
Reicher <robin.o.reicher@gmail.com>
2017-11-18 14:25:43 +0100

    Perhaps?
    Signed-off-by: Reicher <robin.o.reicher@gmail.com>

--- src/boot.js
+++ src/boot.js
@@ -10,8 +10,6 @@ Lettris.Boot.prototype = {
 	// set scale options
 	this.input.maxPointers = 1;
 	this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-	this.scale.pageAlignHorizontally = true;
-	this.scale.pageAlignVertically = true;
 
 	this.game.add.plugin(PhaserInput.Plugin);
 
