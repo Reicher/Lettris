e8b1f041f956996b12a6fb8bf35efebf1a27a103
Reicher <robin.o.reicher@gmail.com>
2017-11-18 14:42:38 +0100

    Revert "Trying with zoom on mobile"
    This reverts commit 3f503927677efc4a2297543df9e9fa9d3ac0d866.

--- src/gameover.js
+++ src/gameover.js
@@ -28,7 +28,6 @@ Lettris.GameOver.prototype = {
 
 	this.input = this.game.add.inputField(50, 100);
 	this.input.blockInput = false
-	this.input.zoom = true;
 	this.input.startFocus()
 	this.record_text = this.game.add.text(this.game.world.centerX,
 					      this.input.y - 25,
