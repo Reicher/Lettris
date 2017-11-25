9f07232eec503b6efefe52f8f5a4d7c8e4926aab
Reicher <robin.o.reicher@gmail.com>
2017-11-25 01:53:19 +0100

    Open Beta 0.11!
    Signed-off-by: Reicher <robin.o.reicher@gmail.com>

--- src/boot.js
+++ src/boot.js
@@ -13,7 +13,7 @@ Lettris.Boot.prototype = {
 	this.scale.pageAlignHorizontally = true;
 	this.scale.pageAlignVertically = true;
 
-	this.game.version = "0.10 Open Beta"
+	this.game.version = "0.11 Open Beta"
 
 	// start the Preloader state
 	this.state.start('Preloader');
