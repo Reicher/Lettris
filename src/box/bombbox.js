17bdec115b8c716d8dfa6abed90ae2fb880bb8e0
Reicher <robin.o.reicher@gmail.com>
2017-11-26 13:53:42 +0100

    Explosion now removes blocks
    Signed-off-by: Reicher <robin.o.reicher@gmail.com>

--- src/box/bombbox.js
+++ src/box/bombbox.js
@@ -12,8 +12,10 @@ BombBox = function (game, id, tile, boxes, x, y) {
 BombBox.prototype = Object.create(Box.prototype);
 BombBox.prototype.constructor = BombBox;
 
-BombBox.prototype.remove = function () {
-    this.BOOM()
+BombBox.prototype.remove = function (boom = true) {
+    if(boom)
+	this.BOOM()
+
     var expand = this.game.add.tween(this.scale).to({x: 1.2, y: 1.2},
 						    200,
 						    Phaser.Easing.Quadratic.In,
@@ -33,22 +35,35 @@ BombBox.prototype.BOOM = function (){
 
         var angle = this.position.angle(box);
 	var dist = this.position.distance(box);
-        //Set the velocity to the angle multiplied by force and the mass
-        //We can also check the distance and multiply with that factor, so near objects have greater velocity
-        //and farther objects have lower velocity
-
 	var force = 2000 / Math.pow(dist, 2)
+
+	if( dist < 60){
+	    box.mark(true)
+	    box.remove(false)
+	}
 
 	box.body.applyImpulseLocal([Math.cos(angle)*force*box.body.mass,
 				    Math.sin(angle)*force*box.body.mass],
 				   box.x,
 				   box.y)
-    }
-    //Additional logic for moving the crates can be applied here if required..
 
-    //For screen shake
-    // var t = game.time.create(true)
-    // t.repeat(20,10,shake,this);
-    // t.start();
-    // t.onComplete.addOnce(resetCam,this);
+	// For screen shake
+	// var t = this.game.time.create(true)
+	// t.repeat(20,10,this.shake,this);
+	// t.start();
+	// t.onComplete.addOnce(this.resetCam,this);
+    }
+
+    BombBox.prototype.resetCam = function(){
+        //Reset camera after shake
+        this.game.camera.x = 2;
+        this.game.camera.y = 2;
+    }
+
+    BombBox.prototype.shake = function(){
+        var min = -2;
+        var max = 2;
+        this.game.camera.x+= Math.floor(Math.random() * (max - min + 1)) + min;
+        this.game.camera.y+= Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 }
