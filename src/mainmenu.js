d4d090fa6751d8f4955b6a2ef383534157f2600d
Reicher <robin.o.reicher@gmail.com>
2017-11-30 07:24:35 +0100

    Add new blinking lights to mainmenu
    Signed-off-by: Reicher <robin.o.reicher@gmail.com>

--- src/mainmenu.js
+++ src/mainmenu.js
@@ -8,6 +8,12 @@ Lettris.MainMenu.prototype = {
 	this.logo = this.game.add.sprite(this.game.width/2, 140, 'sprites', 'logo')
 	this.logo.anchor.setTo(0.5)
 
+	this.addLight(61, 48)
+	this.addLight(403, 48)
+	this.addLight(61, 215)
+	this.addLight(403, 215)
+	this.addLight(314, 56)
+
 	this.boxes = 0
 	this.addMenuButton("Play", 'Game')
 	this.addMenuButton("Language", 'LangMenu')
@@ -20,7 +26,12 @@ Lettris.MainMenu.prototype = {
 					  style)
 	this.version.anchor.setTo(1, 1)
     },
-
+    addLight: function(x, y) {
+	var light = this.game.add.sprite(x, y, 'sprites', 'lamp-off')
+	var speed = this.game.rnd.integerInRange(3, 6)
+	light.animations.add('blink', ['lamp-off', 'lamp-on'], speed, true);
+	light.animations.play('blink');
+    },
     addMenuButton: function (text, next_state) {
 	var button = this.game.add.button(this.game.width/2,
 					  320 + (this.boxes * 120),
