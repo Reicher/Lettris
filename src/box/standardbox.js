c0de5513c6c912c75a8431e60d380a9baf15ae34
Reicher <robin.o.reicher@gmail.com>
2017-11-30 16:52:17 +0100

    new random standardbox
    Signed-off-by: Reicher <robin.o.reicher@gmail.com>

--- src/box/standardbox.js
+++ src/box/standardbox.js
@@ -1,5 +1,7 @@
 StandardBox = function (game, id, tile, x, y) {
-    Box.call(this, game, id, 'box', tile.letter, tile.points, x, y)
+    var candidates = ['box_gold', 'box_paper', 'box_wood']
+    var key = game.rnd.pick(candidates)
+    Box.call(this, game, id, key, tile.letter, tile.points, x, y)
 }
 
 StandardBox.prototype = Object.create(Box.prototype);
