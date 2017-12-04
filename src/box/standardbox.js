bf9050e436301a350f0829b8a846d9f8f8c9a3de
Reicher <robin.o.reicher@gmail.com>
2017-12-04 16:51:07 +0100

    Added standardbox remove animation
    Signed-off-by: Reicher <robin.o.reicher@gmail.com>

--- src/box/standardbox.js
+++ src/box/standardbox.js
@@ -1,6 +1,17 @@
 StandardBox = function (game, id, tile, x, y) {
     Box.call(this, game, id, 'box', tile.letter, tile.points, 1, 1, x, y)
+    var frames = Phaser.Animation.generateFrameNames('box-break', 1, 3)
+    this.animations.add('break', frames, 15);
 }
 
 StandardBox.prototype = Object.create(Box.prototype);
-StandardBox.prototype.constructor = GoldBox;
+StandardBox.prototype.constructor = StandardBox;
+
+StandardBox.prototype.remove = function () {
+    this.animations.play('break')
+    this.text.destroy()
+    this.point_text.destroy()
+    this.animations.currentAnim.onComplete.addOnce(function () {
+	this.destroy()
+    }, this);
+}
