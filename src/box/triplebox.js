dbe9e72e17ebca2f7b467f9e93d239b7828e6747
Reicher <robin.o.reicher@gmail.com>
2017-12-08 00:15:42 +0100

    Actually added trippleboxes
    Signed-off-by: Reicher <robin.o.reicher@gmail.com>

--- src/box/triplebox.js
+++ src/box/triplebox.js
@@ -1,5 +1,5 @@
 TripleBox = function (game, id, x, y) {
-    Box.call(this, game, id, 'multi-box-green', "x3", 0, 0, 3, x, y)
+    Box.call(this, game, id, 'multi-box', "x3", 0, 0, 3, x, y)
     this.point_text.destroy()
     this.text.fontSize = "30px"
 
@@ -8,6 +8,8 @@ TripleBox = function (game, id, x, y) {
     this.body.addPolygon( {} , [ [74, 38]  ,  [38, 0],  [0, 38]  ,  [38, 74] ]);
 
     this.anchor.setTo(0.5) // anchor gets fucked up after clear shape
+
+    Box.prototype.setCoolRemove.call(this, "multi-box-red-break", 3, ['confetti-red'])
 }
 
 TripleBox.prototype = Object.create(Box.prototype);
