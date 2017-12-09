98e517b65c4161ea0ea1af0558b6cc27b1c29a94
Reicher <robin.o.reicher@gmail.com>
2017-12-09 21:14:27 +0100

    position text inside minipanel
    Signed-off-by: Reicher <robin.o.reicher@gmail.com>

--- src/minipanel.js
+++ src/minipanel.js
@@ -2,7 +2,7 @@ MiniPanel = function (game, text, x, y) {
     Phaser.Group.call(this, game);
 
     var style = { font: "25px Arial", align: "center" };
-    this.label = game.add.text(x, y, text, style)
+    this.label = game.add.text(x, y+5, text, style)
     this.label.anchor.setTo(0.5)
 
     var middle = this.game.add.tileSprite(x, y, this.label.width-50,
