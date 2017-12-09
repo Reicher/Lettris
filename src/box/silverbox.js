f4c29629a0d78baf44293742354c870ff2f1230b
Reicher <robin.o.reicher@gmail.com>
2017-12-09 01:28:46 +0100

    Add forgoten silver and gold content
    Signed-off-by: Reicher <robin.o.reicher@gmail.com>

--- src/box/silverbox.js
+++ src/box/silverbox.js
@@ -1,8 +1,8 @@
 SilverBox = function (game, id, tile, x, y) {
     Box.call(this, game, id, 'silver-box', tile.letter, tile.points, 2, 1, x, y)
 
-    Box.prototype.setCoolRemove.call(this, "silver-box-break", 3, ['board1',
-								   'board2',
+    Box.prototype.setCoolRemove.call(this, "silver-box-break", 3, ['silver-board1',
+								   'silver-board2',
 								   'screw1',
 								   'confetti-blue'])
 }
