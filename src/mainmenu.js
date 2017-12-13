72edfbcab8ef8dd4cbe2b9ee804ad17b6240f520
Reicher <robin.o.reicher@gmail.com>
2017-12-13 20:08:19 +0100

    Added Random tip!
    Signed-off-by: Reicher <robin.o.reicher@gmail.com>

--- src/mainmenu.js
+++ src/mainmenu.js
@@ -14,6 +14,35 @@ Lettris.MainMenu.prototype = {
 	this.addLight(this.logo, 163, 75)
 	this.addLight(this.logo, 74, -84)
 
+	// Tip of the day
+	var tips = ["The red button in the lower left corner clears current word.",
+		    "Gold boxes are worth three times normal!",
+		    "Silver boxes are worth twice as much as regular boxes!",
+		    "Multiboxes multiplies the whole word two or three times!!",
+		    "Bomb boxes are allways worth 0 points."]
+	var style = { font: "22px Arial", fill: "#EEEEEE"}
+	this.tipHeader = this.game.add.text(this.game.world.centerX,
+					    this.logo.bottom + 50,
+					    "Random Tip:",
+					    style)
+	this.tipHeader.anchor.setTo(0.5, 1)
+	this.tipHeader.fontWeight = 'bold';
+	this.tipHeader.stroke = '#000000';
+	this.tipHeader.strokeThickness = 3;
+
+	var style = { font: "22px Arial", fill: "#EEEEEE", wordWrap: true, wordWrapWidth: this.game.world.width - 40 };
+	this.tip = this.game.add.text(this.game.world.centerX,
+				      this.tipHeader.bottom,
+				      "\"" + Phaser.ArrayUtils.getRandomItem(tips) + "\"",
+				      style)
+	this.tip.fontWeight = 'italic';
+	this.tip.stroke = '#000000';
+	this.tip.strokeThickness = 3;
+
+	this.tip.anchor.setTo(0.5, 0)
+
+
+	// Buttons
 	this.boxes = 0
 	this.addMenuButton("PLAY", 'Game')
 	this.addMenuButton("CREDITS", 'Credits')
@@ -38,7 +67,7 @@ Lettris.MainMenu.prototype = {
 
 	var choice = new TextButton(this.game,
 				    text, this.game.world.centerX,
-				    400 + (this.boxes * 120),
+				    440 + (this.boxes * 110),
 				    this.startState,
 				    this)
 	choice.button.state = next_state
