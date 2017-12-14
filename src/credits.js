8a8023ea1446b99e81c954a7fa13a1dd2c3e2be8
Reicher <robin.o.reicher@gmail.com>
2017-12-14 16:30:09 +0100

    Added credits
    Signed-off-by: Reicher <robin.o.reicher@gmail.com>

--- src/credits.js
+++ src/credits.js
@@ -8,11 +8,49 @@ Lettris.Credits.prototype = {
 	this.logo = this.game.add.sprite(this.game.width/2, 140, 'sprites', 'logo')
 	this.logo.anchor.setTo(0.5)
 
+	this.category(this.logo.bottom+50,
+		      "Programmers",
+		      ["Robin Reicher", "Magnus Malm", "Andreas Galic"])
+
+	this.category(this.logo.bottom+250,
+		      "Art",
+		      ["David Levi"])
+
 	new TextButton(this.game, "Main Menu",
 		       this.game.world.centerX,
 		       this.game.world.height-70,
 		       this.back,
 		       this)
+    },
+    category: function(y, title, names) {
+	var header_style = { font: "30px Verdana",
+			     fontWeight: 'bold',
+			     stroke: '#000000',
+			     strokeThickness: 2,
+			     fill: "#EEEEEE",
+			     align: "center"};
+
+	var name_style = { font: "25px Verdana",
+			     fontWeight: 'italic',
+			     stroke: '#000000',
+			     strokeThickness: 2,
+			     fill: "#EEEEEE",
+			   align: "center"};
+
+	var header = this.game.add.text(this.game.world.centerX,
+					y,
+					title,
+					header_style)
+	header.anchor.setTo(0.5)
+
+	for(i = 0; i < names.length; ++i){
+	    var cast = this.game.add.text(this.game.world.centerX,
+					  header.bottom + 20 + i * 30,
+					  names[i],
+					  name_style)
+	    cast.anchor.setTo(0.5)
+	}
+
     },
     back: function() {
 	this.state.start('MainMenu');
