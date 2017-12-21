83a75d74b8370f914ab1f858b7b163bc9943afa7
Reicher <robin.o.reicher@gmail.com>
2017-12-21 17:01:34 +0100

    0.24 RC
    Signed-off-by: Reicher <robin.o.reicher@gmail.com>

--- src/boot.js
+++ src/boot.js
@@ -13,7 +13,7 @@ Lettris.Boot.prototype = {
 	this.scale.pageAlignHorizontally = true;
 	this.scale.pageAlignVertically = true;
 
-	this.game.version = "0.23 RC"
+	this.game.version = "0.24 RC"
 
 	// start the Preloader state
 	this.state.start('Preloader');
