f5dd73af5153fe463a33477df703bcf3b5d147b8
Reicher <robin.o.reicher@gmail.com>
2017-12-23 00:49:20 +0100

    Release 1.0! Happy Holidays!
    Signed-off-by: Reicher <robin.o.reicher@gmail.com>

--- src/boot.js
+++ src/boot.js
@@ -13,7 +13,7 @@ Lettris.Boot.prototype = {
 	this.scale.pageAlignHorizontally = true;
 	this.scale.pageAlignVertically = true;
 
-	this.game.version = "0.24 RC"
+	this.game.version = "Version 1.0"
 
 	// start the Preloader state
 	this.state.start('Preloader');
